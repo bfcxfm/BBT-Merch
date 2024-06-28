b05fd5d17a9088bc63196cdc75d7c650765cded4
“bfcxfm” <bfcxfm@gmail.com>
2024-06-28 10:57:02 +0800

    Update Drinks Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
+import DrinksPage from "./components/DrinksPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-extrabold underline">BBT + Merch</h1>
-      <div className="card">
+      <div>
+        <DrinksPage />
+      </div>
+      {/* <h1 className="text-3xl font-extrabold underline">BBT + Merch</h1> */}
+
+      {/* <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>
           {" "}
           <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
           BBT count is {count}
         </Button>
-      </div>
+      </div> */}
     </>
   );
 }
---
+++ src/components/DrinksPage/index.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+// import "./drinks.css";
+
+// Utility function to format the drink type
+const formatOption = (option) => {
+  return option.toLowerCase().replace(/\s/g, "-");
+};
+
+const DrinksPage = () => {
+  const [selectedDrink, setSelectedDrink] = useState("black");
+
+  const drinkOptions = [
+    "Black",
+    "Flat White",
+    "Latte",
+    "Cappuccino",
+    "Americano",
+    "Espresso",
+    "Doppio",
+    "Cortado",
+    "Macchiato",
+    "Mocha",
+    "Affogato",
+    "Con Panna",
+    "Irish",
+    "Cafe Au Lait",
+    // Repeats
+    // "Black",
+    // "Flat White",
+    // "Latte",
+    // "Cappuccino",
+    // "Americano",
+    // "Espresso",
+    // "Doppio",
+    // "Cortado",
+    // "Macchiato",
+    // "Mocha",
+    // "Affogato",
+    // "Con Panna",
+    // "Irish",
+    // "Cafe Au Lait",
+    // "Black",
+    // "Flat White",
+    // "Latte",
+    // "Cappuccino",
+    // "Americano",
+    // "Espresso",
+    // "Doppio",
+    // "Cortado",
+  ];
+
+  return (
+    <div className="drinks-page grid grid-cols-5 gap-4">
+      <div className="wrapper col-start-2 col-span-2 p-4">
+        <div className="shadow"></div>
+        <div className="title capitalize">
+          {selectedDrink.replace("-", " ")}
+        </div>
+        <div className={`cup ${selectedDrink}`}>
+          <div className="contents">
+            <div className="gelato">gelato</div>
+            <div className="foam">milk foam</div>
+            <div className="cream">cream</div>
+            <div className="steamed-milk">steamed milk</div>
+            <div className="milk">milk</div>
+            <div className="chocolate">chocolate</div>
+            <div className="sugar">sugar</div>
+            <div className="whiskey">whiskey</div>
+            <div className="water">water</div>
+            <div className="coffee">coffee</div>
+            <div className="espresso">
+              <span>(2)&nbsp;</span>espresso
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="options col-start-5 col-span-1 p-4 overflow-auto">
+        {drinkOptions.map((option, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              setSelectedDrink(formatOption(option));
+            }}
+            className="cursor-pointer py-2"
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DrinksPage;
