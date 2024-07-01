6f2efba8e24e5c807396f950184bd21cf4330baa
“bfcxfm” <bfcxfm@gmail.com>
2024-07-01 20:10:07 +0800

    Update Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,57 @@ import "./App.css";
 import { Button } from "@/components/ui/button";
 import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
 import DrinksPage from "./components/DrinksPage";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+import Dashboard from "./components/AdminPage";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { Route, Routes } from "react-router-dom";
+import OrderPage from "./components/OrderPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <DrinksPage />
-      </div>
-      {/* <h1 className="text-3xl font-extrabold underline">BBT + Merch</h1> */}
-
-      {/* <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          {" "}
-          <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
-          BBT count is {count}
-        </Button>
-      </div> */}
-    </>
+    <div className="grid grid-rows-5 grid-cols-5 gap-4">
+      <Routes>
+        <Route
+          path="/admin"
+          element={
+            <div className="col-span-5">
+              <TooltipProvider>
+                <Dashboard />
+              </TooltipProvider>
+            </div>
+          }
+        />
+        <Route
+          path="/user"
+          element={
+            <div className="col-span-5">
+              <TooltipProvider>
+                <OrderPage />
+              </TooltipProvider>
+            </div>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="row-start-1 col-span-2">
+                <LoginForm />
+              </div>
+              <div className="row-start-1 col-start-3 col-span-2">
+                <SignUpForm />
+              </div>
+              <div className="row-start-2 col-span-5">
+                <DrinksPage />
+              </div>
+            </>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./components/DrinksPage/drinks.css"; // Import specific css for the DrinksPage if necessary
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
