852c59a2396bb5000e8e0d7dd72a0bae91e75d66
“bfcxfm” <bfcxfm@gmail.com>
2024-07-03 19:46:15 +0800

    Update CryptoJS

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,7 +11,60 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+import {
+  getLoginDetails,
+  loginUser,
+  checkLogin,
+  checkPermission,
+} from "/service/users";
+import { hashDataWithSaltRounds, storeToken } from "/util/security";
+
 export default function LoginForm() {
+  const [formState, setFormState] = useState({});
+
+  function handleChange(evt) {
+    var currForm = formState;
+    currForm[evt.target.name] = evt.target.value;
+    setFormState(currForm);
+  }
+
+  async function handleSubmit(evt) {
+    try {
+      evt.preventDefault();
+      // We don't want to send the 'error' or 'confirm' property,
+      //  so let's make a copy of the state object, then delete them
+      // highlight-start
+      const formData = { ...formState };
+      delete formData.error;
+      delete formData.confirm;
+      // highlight-end
+      console.log(formData);
+      // get user salt and iterations from database
+      const loginDetails = await getLoginDetails(formData.email);
+      console.log(loginDetails);
+      const hashedPassword = hashDataWithSaltRounds(
+        formData.password,
+        loginDetails.salt,
+        loginDetails.iterations
+      );
+      formData.password = hashedPassword;
+      console.log(formData);
+      const token = await loginUser(formData);
+      // store token in localStorage
+      storeToken(token);
+      // Baby step!
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function handleCheckLogin() {
+    await checkLogin();
+  }
+  async function handleCheckPermission() {
+    await checkPermission();
+  }
+
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -28,6 +81,7 @@ export default function LoginForm() {
               id="email"
               type="email"
               placeholder="m@example.com"
+              onChange={handleChange}
               required
             />
           </div>
@@ -35,7 +89,13 @@ export default function LoginForm() {
             <div className="flex items-center">
               <Label htmlFor="password">Password</Label>
             </div>
-            <Input id="password" type="password" required />
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              onChange={handleChange}
+              required
+            />
           </div>
           <Button type="submit" className="w-full">
             Login
@@ -43,7 +103,7 @@ export default function LoginForm() {
         </div>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
+          <Link to="/signup" className="underline">
             Sign up
           </Link>
         </div>
